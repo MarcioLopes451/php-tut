c81c8172b204552445bbf44cf90c8ead7e6592bd
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2024-08-13 20:47:11 +0100

    add: php work

--- isset.php
+++ isset.php
@@ -1,3 +1,29 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['login'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if (empty($username)) {
+            echo 'Username is missing.';
+        } elseif (empty($password)) {
+            echo 'Password is missing.';
+        } else {
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $username;
+        }
+    }
+
+    if (isset($_POST['logout'])) {
+        session_destroy();
+        header('Location: ' . $_SERVER['PHP_SELF']);
+        exit;
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,34 +34,23 @@
 </head>
 
 <body>
-    <p>login page</p>
-    <form action="isset.php" method="post">
-        <label>username</label>
-        <input type='text' name='username' />
-        <br>
-        <label>password</label>
-        <input type='password' name='password' />
-        <br>
-        <input type='submit' value="login" name='login' />
-    </form>
+    <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true): ?>
+        <p>login success</p>
+        <p> hello <?php echo $_SESSION['username'] ?></p>
+        <form action='isset.php' method="post">
+            <input type='submit' name="logout" value="logout" />
+        </form>
+    <?php else: ?>
+        <form action="isset.php" method="post">
+            <label>username</label>
+            <input type='text' name='username' />
+            <br>
+            <label>password</label>
+            <input type='password' name='password' />
+            <br>
+            <input type='submit' value="login" name='login' />
+        </form>
+    <?php endif; ?>
 </body>
 
 </html>
-<?php
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-if (isset($_POST['login'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    if (empty($username)) {
-        echo 'username is missing.';
-    } elseif (empty($password)) {
-        echo 'password is missing.';
-    } else {
-        echo "hello {$username}";
-    }
-}
-
-?>
